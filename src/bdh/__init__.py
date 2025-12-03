3f3e3cce94559d5602dc2dd92ee8879ce98c6f8e
Bahlmann, Jörg <joerg.bahlmann@easy-software.com>
2025-12-03 14:51:38 +0100

    updated model imports

--- src/bdh/__init__.py
+++ src/bdh/__init__.py
@@ -1,12 +1,8 @@
 from .model import BDH, BDHConfig
 from .classifier import BDHClassifier  # For text classification (SST-2), not used for market data
-from .market import (
-    MarketBDHBase,
-    MarketBDHPretrain,
-    MarketBDH,
-    MarketBDHConfig,
-    load_pretrained_weights
-)
+from .market_bidirect import MarketBDHBase, MarketBDHConfig
+from .market_bidirect_pretrain import MarketBDHPretrain
+from .market_bidirect_finetune import MarketBDH, load_pretrained_weights
 from .data import MarketDataset
 
 __all__ = [
